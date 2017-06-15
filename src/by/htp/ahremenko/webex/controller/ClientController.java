07110e1622fa778d82a54b3142d935059e477849
Lelik <Lelik@ahremenko>
2017-06-15 18:36:57 +0200

    Cleint v.0.3

--- src/by/htp/ahremenko/webex/controller/ClientController.java
+++ src/by/htp/ahremenko/webex/controller/ClientController.java
@@ -9,6 +9,8 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import by.htp.ahremenko.utl.KeyboardInput;
+
 public class ClientController {
 	private String netAress;
 	private int port;
@@ -22,6 +24,46 @@ public class ClientController {
 	
 	public void start() throws IOException {
 		try {
+			InputStream in = socket.getInputStream();
+			OutputStream out = socket.getOutputStream();
+				
+			DataInputStream dataIn = new DataInputStream (in);
+			DataOutputStream dataOut = new DataOutputStream(out);
+			
+			String response;
+			String request;
+			
+			KeyboardInput kbd = new KeyboardInput();
+	        
+	        // examples and fill table of cars
+	        
+	        /*try {
+	        	ViewExamples.showExamples();
+	        } catch (LogicException e) {
+	        	System.err.println(e.getMessage());
+	        }*/
+	        
+	        do {
+	        	request = kbd.enterString("Type command here >> ");
+	        	if (request.toUpperCase().equals("EXIT")) 
+	        		break;
+	        	try {
+	        		dataOut.writeUTF(request);
+	    			dataOut.flush();  // push all from buffer
+	    			
+	    				response = dataIn.readUTF();
+	    				//System.out.println(response);
+	    		} catch (EOFException e) {
+	    				response = "";
+	    				//System.out.println("response is NULL");
+	    		}
+	        		//response = controller.executeTask(commandLine);
+	        	if (!response.equals("")) System.out.println("\n" + response);
+	        } while (!request.toUpperCase().equals("EXIT"));  
+	        
+	        System.out.println("\nHave a nice day :)");
+
+			/*
 			System.out.println("[Client] got a connection");
 			
 			InputStream in = socket.getInputStream();
@@ -51,7 +93,7 @@ public class ClientController {
 				System.out.println("[Client] Server sent a response 2: " + response);
 			} catch (EOFException e) {
 				System.out.println("[Client] gets NULL answer.");
-			}
+			}*/
 			
 			
 		} finally {
